eaf9226825bd64d633b74b37c7a40dc2a9cd1f53
Ki Yung Ahn <kyagrd@gmail.com>
2022-08-19 02:27:46 +0900

    save

---
+++ run.sh
@@ -0,0 +1,11 @@
+LAB="--env JUPYTER_ENABLE_LAB=yes"
+IMG=jbindinga/java-notebook
+TAG=latest
+HOSTPORT=8888
+
+docker pull $IMG:$TAG
+docker run --rm -p $HOSTPORT:8888 $LAB \
+ 	-v "$PWD":/home/jovyan/work \
+ 	--env JUPYTER_TOKEN=x --name java_notebook \
+       	$IMG:$TAG
+
